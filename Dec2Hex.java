76b15f2eedd8ebf469d5eef0b944af74e5be001c
Ubuntu <ubuntu@ip-172-31-33-47.ec2.internal>
2020-11-22 22:05:14 +0000

    Dec2Hex fix

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 class Dec2Hex
-{
 
+{
     public static int Arg1;
     public static void main(String args[])
     {
@@ -14,25 +14,34 @@ class Dec2Hex
             } 
             catch (NumberFormatException e) 
             {
-                System.err.println("Argument" + args[0] + " must be an integer.");
+                System.err.println("Argument :" + args[0] + "  must be an integer.");
                 System.exit(1);
             }
         }
+	else
+	{
+		System.err.println("Cannot leave empty. Please input a valid number");
+		System.exit(1);
+}
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
          int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
+
+	    num = Arg1;
+
+	    String hexadecimal=""; 
+
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-        
+
         while(num != 0)
         {
 	    rem=num%16;
 	    hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-        
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
 
+        System.out.println("Hexadecimal representation is  : " + hexadecimal);
     }
-} 
+
+}
