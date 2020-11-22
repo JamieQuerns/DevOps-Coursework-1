b4822a5a6c9b6e71fd8279d9fc1fb8fa78782734
Ubuntu <ubuntu@ip-172-31-33-47.ec2.internal>
2020-11-22 22:32:47 +0000

    fix

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -23,15 +23,14 @@ class Dec2Hex
 		System.err.println("Cannot leave empty. Please input a valid number");
 		System.exit(0);
 }
+ 
+      	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
-         int rem, num;
+           int rem, num;
 
 	    num = Arg1;
 
-	    String hexadecimal=""; 
-
+	String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
