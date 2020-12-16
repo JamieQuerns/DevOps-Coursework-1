7edff98a953c6c6e56d251da22b9f366b5de8856
Ubuntu <ubuntu@ip-172-31-33-47.ec2.internal>
2020-12-16 00:26:06 +0000

    Ammended Dec2Hex

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -28,8 +28,7 @@ class Dec2Hex
       	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
            int rem, num;
-
-	    num = Arg1;
+	   num = Arg1;
 
 	String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
